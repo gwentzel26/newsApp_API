67062c72b7575a6b8833c412987108dcb0b23c22
Gavin Wentzel <gavinwentzel@gmail.com>
2022-07-25 15:24:29 -0600

    Completed auth routes

--- server/routes/auth-routes.js
+++ server/routes/auth-routes.js
@@ -6,4 +6,29 @@ const { login,
     forgotPassword,
     resetPassword,
     changePassword } = require("../controllers/authorization");
+
+    router.post("/login", async(req, res) => {
+        await login(req.body, res);
+    });
     
+    router.post("/register", async(req, res) => {
+        await register(req.body, "user", res);
+    });
+
+    router.post("/verifyEmail", async(req, res) => {
+        await verifyEmail(req.body, res);
+    });
+
+    router.post("/forgotPassword", async(req, res) => {
+        await forgotPassword(req.body, res);
+    });
+
+    router.post("/resetPassword", async(req, res) => {
+        await resetPassword(req.body, res);
+    });
+
+    router.post("/changePassword", ensureAuthenticated, async(req, res) => {
+        await changePassword(req.body, res);
+    });
+
+    module.exports = router;
