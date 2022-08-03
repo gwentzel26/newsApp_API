9da979fbf8950ed5ac13d692136593c9035e3a11
Gavin Wentzel <gavinwentzel@gmail.com>
2022-08-03 14:18:12 -0600

    Beginning login component react file

--- client/newsapp/src/App.js
+++ client/newsapp/src/App.js
@@ -1,25 +1,24 @@
+import React from 'react';
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+       <BrowserRouter>
+    
+    <Navbar />
+    <div className='container'>
+    <Routes> 
+        <Route path='/' element = {<ExercisesList/>} />
+        <Route path='/edit/:id' element = {<EditExercise/>} />
+        <Route path='/create' element = {<CreateExercise/>} />
+        <Route path='/user' element = {<CreateUser/>} />
+    </Routes> 
     </div>
+    </BrowserRouter>
+    </>
   );
 }
-
 export default App;
---
+++ client/newsapp/src/components/login-template.js
@@ -0,0 +1,24 @@
+import React, {Component} from "react";
+import { Link } from "react-router-dom";
+
+export default class Login extends Component {
+    render(){
+        <div className="card">
+            <input className="email"></input>
+            <input className="password"></input>
+            <button></button>
+
+
+
+        </div>
+
+
+
+
+
+
+    }
+
+
+
+}
