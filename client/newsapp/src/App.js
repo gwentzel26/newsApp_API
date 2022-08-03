7634037b5bd7cd31c17ea923ff9b1edda5d15420
Gavin Wentzel <gavinwentzel@gmail.com>
2022-08-03 16:32:34 -0600

    Centered elements of the login form and added styling and animation to forgot password link

--- client/newsapp/src/App.js
+++ client/newsapp/src/App.js
@@ -2,19 +2,21 @@ import React from 'react';
 import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/login-template';
 
 function App() {
   return (
     <>
        <BrowserRouter>
     
-    <Navbar />
+    {/* <Navbar /> */}
+    
     <div className='container'>
     <Routes> 
-        <Route path='/' element = {<ExercisesList/>} />
-        <Route path='/edit/:id' element = {<EditExercise/>} />
+        <Route path='/' element = {<Login/>} />
+        {/* <Route path='/edit/:id' element = {<EditExercise/>} />
         <Route path='/create' element = {<CreateExercise/>} />
-        <Route path='/user' element = {<CreateUser/>} />
+        <Route path='/user' element = {<CreateUser/>} /> */}
     </Routes> 
     </div>
     </BrowserRouter>
--- client/newsapp/src/components/login-template.js
+++ client/newsapp/src/components/login-template.js
@@ -1,31 +1,33 @@
 import React, {Component} from "react";
 import { Link } from "react-router-dom";
-const { login, 
-    register,
-    verifyEmail,
-    forgotPassword,
-    resetPassword,
-    changePassword } = require("../../../../server/controllers/authorization");
+import "../css/loginForm.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+// const { login, 
+//     register,
+//     verifyEmail,
+//     forgotPassword,
+//     resetPassword,
+//     changePassword } = require("../../../../server/controllers/authorization");
 
 export default class Login extends Component {
     render(){
-        <div className="card">
-            <input className="email"></input>
-            <input className="password"></input>
-            <button onClick={login}>Login</button>
-            <Link to="/forgotPassword">Forgot password?</Link>
-            <button>Create new account
+        return(
+        <div className="card" style={{width: "40%"}}>
+            <input type="text" className="email" placeholder="email"></input>
+            <input type="text" className="password" placeholder="password"></input>
+            <button className="loginBtn" style={{width: "85%"}} >Login</button>
+            <div className="fgtPswdCont" style={{width: "50%"}}>
+                <Link className="fgtPswd hover-underline-animation" to="/forgotPassword">Forgot password?</Link>
+            </div>
+           
+            
+            
+            <button className="registerBtn">Create new account
                 <Link to="/register"></Link>
             </button>
         </div>
-
-
-
-
-
-
+        )
+      
     }
-
-
-
 }
+
